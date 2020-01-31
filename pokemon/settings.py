c1e5e029742023fd9d0f05f4e0bdc008023e191b
jumpingchu <jumpingchu@gmail.com>
2020-01-31 16:43:10 +0800

    v1.0

--- pokemon/settings.py
+++ pokemon/settings.py
@@ -110,6 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
-os.makedirs(STATIC_TMP, exist_ok=True)
+#STATIC_TMP = os.path.join(BASE_DIR, 'static')
+#os.makedirs(STATIC_TMP, exist_ok=True)
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
