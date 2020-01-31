ae76ff4078cb150491569abf0a8fe11f550aa622
jumpingchu <jumpingchu@gmail.com>
2020-01-31 16:44:54 +0800

    v1.0

--- pokemon/settings.py
+++ pokemon/settings.py
@@ -111,6 +111,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATIC_TMP = os.path.join(BASE_DIR, 'static')
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 #os.makedirs(STATIC_TMP, exist_ok=True)
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
